267ad29839a8dc81f360a5b3ddce792be38399e0
sachsstudent1 <132019247+sachsstudent1@users.noreply.github.com>
2023-05-06 23:24:54 -0600

    Add files via upload

---
+++ helloworld.sql
@@ -0,0 +1,5 @@
+CREATE TABLE helloworld (phrase TEXT);
+INSERT INTO helloworld VALUES ("Hello, World!");
+INSERT INTO helloworld VALUES ("Goodbye, World!");
+SELECT COUNT(*) FROM helloworld;
+SELECT * FROM helloworld WHERE phrase = "Hello, World!";
---
+++ helloworld.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react'
+const HelloWorld: FC = () => "Hello, World!"
+export default HelloWorld
